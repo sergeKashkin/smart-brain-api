62938b61b738e59fbe0a7313b0a2e27fb33a87a2
sergeKash <39168159+sergeKashkin@users.noreply.github.com>
2020-05-25 15:42:22 +0300

    fixing

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,10 @@ const handleRegister = (req,res, db, bcrypt) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('Unable to register'));
+    .catch(err => {
+        console.log('transaction error');
+        res.status(400).json('Unable to register');
+    });
 }
 
 module.exports = {
