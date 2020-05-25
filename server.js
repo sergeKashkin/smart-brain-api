4f59b477dc1756030dcec2f72a8f9ccaf7b1a8fc
sergeKash <39168159+sergeKashkin@users.noreply.github.com>
2020-05-25 14:55:28 +0300

    connecting DB

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
+        host: 'postgresql-amorphous-45197',
         user: 'postgres',
         password: 's230694K',
         database: 'smart-brain'
