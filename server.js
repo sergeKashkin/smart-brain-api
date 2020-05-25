486e0aa5f832e39e615170280ce55d4bcbd370ff
sergeKash <39168159+sergeKashkin@users.noreply.github.com>
2020-05-25 15:13:48 +0300

    fixing DB connection 2

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
     }
 })
 
